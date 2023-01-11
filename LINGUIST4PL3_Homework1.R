9f71f5ada046ee46bb860c32c37552797f94380d
repisopg <113913990+repisopg@users.noreply.github.com>
2023-01-11 08:00:29 -0500

    Add files via upload

---
+++ LINGUIST4PL3_Homework1.R
@@ -0,0 +1,83 @@
+# LINGUIST 4PL3 - Advanced Programming for Linguists
+# Homework 0
+
+# Name: 
+# Repository: 
+
+# Learning Objectives:
+	# Understand the difference between git and GitHub
+	# Identify the difference between local and remote repositories
+	# Fork the Advanced Programming repository 
+	# Commit changes and submit a pull request
+	# Learn about GitHub markdown syntax
+	
+# Associated Resources:
+	# GitHub docs has lots of information on getting started with GitHub:
+		# https://docs.github.com/en/get-started/quickstart 
+
+	# A video detailing the differences between git and GitHub as well as cloning, 
+	# forking, commits and pull requests
+		# https://www.youtube.com/watch?v=DVRQoVRzMIY
+
+#Instructions:
+# PART 1 - Create a GitHub account
+	# Navigate to https://github.com and follow the prompts
+	# Select GitHub free and verify your email address
+
+	# See: https://docs.github.com/en/get-started/onboarding/getting-started-with-your-github-account
+
+
+# PART 2 - Create a Hello World repository and commit
+	# Use the '+' drop down menu to select New repository
+	# Initialize the repository with a README file, and create
+	# Open and edit the README file to add your name, program and year
+	# Preview changes, add a message and commit to the main branch
+	
+	# See: https://docs.github.com/en/get-started/quickstart/hello-world
+
+
+# PART 3 - Create a personal copy of the course repository
+
+	# Advanced Programming repository: https://github.com/repisopg/AdvancedProgramming.git
+	
+	# Fork the Advanced Programming repository
+		# Navigate to repository you want to clone (the course repository) and click 'Fork'
+		# You can rename the fork and add a description
+		# Generate fork
+		# Now you have a copy of the course repository on your personal GitHub account
+	
+		# See: https://docs.github.com/en/get-started/quickstart/fork-a-repo
+	
+	# Clone your copy of the course repository to your local computer on R studio
+
+# PART 4 - Use GitHub markdown syntax
+	# Navigate to your copy of the course repository
+	# Open and edit the README file to add text
+	# Add your name and explore GitHub markdown to add each of the following:
+			# A large heading
+			# An emoji
+			# A text quote
+	# Commit your changes to the main branch 
+	#Markdown syntax is quite similar to RMarkdown syntax, but read about that on the guidelines:
+	# See: https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+
+
+# PART 5 - Submit a Pull Request
+
+#A pull request – also referred to as a merge request – is an event that takes place in software development when a contributor/developer
+#is ready to begin the process of merging new code changes with the main project repository.
+	# Navigate your remote copy of the course repository
+	# Open and edit the Homework1.R file
+	# Add a link to your file pointing to your copy of the course repository and your name at the top where indicated (at the top of this file you will see Name: and Repository:)
+	# Preview your changes and commit to the main branch
+	
+	# Navigate to the course repository on Github and click pull request
+	# Select compare across forks and the course repository as the base repository (main branch)
+	# Enter your copy of the course repository as the head repository (main branch) (enter the link)
+	# Add your name as the title of the pull request and the link to your copy of the course repository in the comments
+	# Click create pull request
+	
+	# See: https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request-from-a-fork
+
+  #For this assignment, you need to upload a copy of this Script on Avenue and submit a pull request before Monday 11:59PM.
+  #Please, ask your questions on the discussion board.
