98b89ef3e27a5ce776aed3eef17d078936478b28
Lucy Thomas <lucyrthomas8@gmail.com>
2023-01-13 21:06:33 -0500

    link and name

--- LINGUIST4PL3_Homework1.R
+++ LINGUIST4PL3_Homework1.R
@@ -1,8 +1,10 @@
 # LINGUIST 4PL3 - Advanced Programming for Linguists
 # Homework 0
 
-# Name: Lucy
+# Name: Lucy Thomas
 # Repository: Hello-World
+# link: https://github.com/lucyrthomas/AdvancedProgramming
+
 
 # Learning Objectives:
 	# Understand the difference between git and GitHub
